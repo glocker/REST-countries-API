f83110278d7d2a14addffbba5c6fec763231e34e
glocker <georgeglocker@gmail.com>
2020-11-08 23:51:24 +0700

    Dark mode is here

--- Client/Frontend.js
+++ Client/Frontend.js
@@ -1,3 +1,16 @@
 function dropDownToggle() {
     document.getElementById("dropdown-content").classList.toggle("show");
 }
+
+function darkModeToggle() {
+    let body = document.getElementById('body');
+    let main = document.getElementById('main');
+
+    if (body.classList.contains('dark') && main.classList.contains('dark')) {
+       body.classList.remove('dark');
+       main.classList.remove('dark'); 
+    } else {
+        body.classList.add('dark');
+        main.classList.add('dark');
+    }
+}
