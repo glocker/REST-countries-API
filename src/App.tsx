b263643677be9bc68caaa4257e4b8fc53fa86a6f
George Glocker <georgeglocker@gmail.com>
2023-02-16 22:39:09 +0400

    react to main

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './MainPage.css';
+import Header from "./Components/Header/Header";
+
+function MainPage() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+export default MainPage;
---
+++ src/Components/Header/Header.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import moon from "../../img/moon.svg";
+import './Header.css';
+
+function Header() {
+    return (
+        <header>
+            <h2>Where in the world?</h2>
+            <div className="dark-mode-container">
+            <button type="button" className="dark-mode-btn">
+                <img src={moon} alt="Dark mode" className="dark-mode-icon" />
+                <div className="dark-mode-title">
+                    Dark Mode
+                </div>
+            </button>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
