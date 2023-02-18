0664c6d411744dbebe43c803be75b3af919d4ef1
George Glocker <georgeglocker@gmail.com>
2023-02-18 20:12:17 +0400

    Countries names is on the page

--- src/Components/CountriesList/CountriesList.tsx
+++ src/Components/CountriesList/CountriesList.tsx
@@ -1,28 +1,38 @@
-import React, { useEffect } from 'react';
+import React, {useEffect, useState} from 'react';
 import './CountriesList.css';
 
 const requestURL = 'https://restcountries.com/v3.1/';
 
 function CountriesList() {
 
+    const [countries, setCountries] = useState([]);
+
     useEffect(() => {
 
-        const restountriesAPI = async () => {
+        const restCountriesAPI = async () => {
             const data = await fetch(requestURL + 'all', {method: 'GET'});
 
             const dataToJSON = await data.json();
 
-            console.log(dataToJSON);
+            console.log(dataToJSON)
+            setCountries(dataToJSON);
         }
 
-        restountriesAPI();
+        restCountriesAPI();
 
     }, [])
 
 
     return(
         <div className="countries-list">
-            Countries list
+            Countries list:
+            {
+                countries.map((countriesData: any) => {
+                    return(
+                        <div key={countriesData.name.official}>{countriesData.name.official}</div>
+                    )
+                })
+            }
         </div>
     )
 }
