e9baf3c1d8778999be30a121d2ada5f919cba944
Egor Rakitin <georgeglocker@gmail.com>
2023-02-23 17:01:40 +0400

    Stuck on search bar

--- src/Components/CountriesList/CountriesList.tsx
+++ src/Components/CountriesList/CountriesList.tsx
@@ -1,12 +1,28 @@
 import React, {useEffect, useState} from 'react';
 import './CountriesList.css';
-import CountryCard from '../CountryCard/CountryCard'
+import CountryCard from '../CountryCard/CountryCard';
 
 const requestURL = 'https://restcountries.com/v3.1/';
 
 function CountriesList(): any {
 
+    function search(items: any) {
+        return items.filter((item: any) => {
+            return searchParam.some((searchValue: any) => {
+                return (
+                    item[searchValue] 
+                    ? item[searchValue].toString().toLowerCase().indexOf(query.toLowerCase()) > -1
+                    : null
+                )
+            })
+        })
+    }
+
     const [countries, setCountries]: any = useState([]);
+
+    const [query, setQuery]: any = useState("");
+
+    const [searchParam]: any = useState(['capital', 'name']);
 
     useEffect(() => {
 
@@ -24,9 +40,24 @@ function CountriesList(): any {
 
 
     return(
-        <div className="countries-list">
-            <CountryCard data={countries} />
-        </div>
+        <main>
+            <div className="searchBar">
+                <label htmlFor="search-form">
+                    <input
+                        type="search"
+                        name="search-form"
+                        id="search-form"
+                        className="search-input"
+                        placeholder="Search for a country..."
+                        value={query}
+                        onChange={(e) => setQuery(e.target.value)}
+                    />
+                </label>
+            </div>
+            <div className="countries-list">
+                <CountryCard data={countries} />
+            </div>
+        </main>
     )
 }
 
